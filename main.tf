86657bda4714b86a875c179c6093d87f8f6b5e82
Michael Braun <michaelbraunbass@gmail.com>
2020-09-22 19:19:54 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
-#This info is required for Github Actions to trigger the Terraform Cloud Deployment
+ terraform {
+  backend "remote" {
+    organization = "MikeNet"
 
-terraform {
-      backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "MikeNet"
-
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "VulnerableAzure"
-         }
-       }
-     }
+    workspaces {
+      name = "VulnerableAzure-AzurePipeline"
+    }
+  }
+}
